f13e66c1c8384f9f9f08a0878f5ac6842ed15534
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-04-11 19:32:58 +0300

    Created Home.tsx

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,16 @@
+import './home.scss';
+
 import * as React from 'react';
 import PageContentWrapper from '../page-content-wrapper';
+import HomeHeader from './home-header';
+import HomeContent from './home-content';
+import HomeFooter from './home-footer';
 
 const Home: React.FC<{}> = () => (
-    <PageContentWrapper>
-        <img src="https://scontent-sof1-1.cdninstagram.com/v/t51.2885-15/e35/120500076_2810435175903781_2103389502367425261_n.jpg?tp=1&_nc_ht=scontent-sof1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=1QSkYFu2EjcAX9l73Ka&edm=AP_V10EAAAAA&ccb=7-4&oh=c5d0bc4c829610e0764edaf0a31b358c&oe=60996F98&_nc_sid=4f375e" />
+    <PageContentWrapper className="home">
+        <HomeHeader />
+        <HomeContent />
+        <HomeFooter />
     </PageContentWrapper>
 );
 
