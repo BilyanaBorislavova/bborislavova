6d1be7f4f04319a711aaaa96f2c12c66a92ae4e5
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-04-11 21:38:20 +0300

    Added more icons

--- src/components/icons/icons.js
+++ src/components/icons/icons.js
@@ -5,6 +5,8 @@ import instagramLogo from '../../assets/icons/instagram.svg';
 import facebookLogo from '../../assets/icons/facebook.svg';
 import githubLogo from '../../assets/icons/github.svg';
 import linkedinLogo from '../../assets/icons/linkedin.svg'
+import graduationHatIcon from '../../assets/icons/graduation-hat.svg';
+import suitcaseIcon from '../../assets/icons/suitcase.svg';
 
 const createIcon = src => props => <SvgIcon src={src} {...props} />;
 
@@ -12,10 +14,14 @@ const InstagramLogo = createIcon(instagramLogo);
 const FacebookLogo = createIcon(facebookLogo);
 const GithubLogo = createIcon(githubLogo);
 const LinkedinLogo = createIcon(linkedinLogo);
+const GraduationHatIcon = createIcon(graduationHatIcon);
+const SuitcaseIcon = createIcon(suitcaseIcon);
 
 export {
     InstagramLogo,
     FacebookLogo,
     GithubLogo,
     LinkedinLogo,
+    GraduationHatIcon,
+    SuitcaseIcon,
 };
