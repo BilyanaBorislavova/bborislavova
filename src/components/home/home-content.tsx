eef50707e04186fb874ff81d5940ff63728bed71
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-04-11 16:07:09 +0300

    Added home-content component

---
+++ src/components/home/home-content.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+const HomeContent: React.FC<{}> = () => (
+    <article className="home-content">
+        <h1 className="home-content-title">
+            Hello, I`m
+            <span className="home-content-title-name">
+                Bilyana Borislavova!
+            </span>
+        </h1>
+        <h3 className="home-content-occupation">
+            Front End Developer
+        </h3>
+        <p className="home-content-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque esse dolor nihil provident totam eum, nemo eius temporibus molestias neque ex earum magnam fugit sunt sed minus assumenda fugiat possimus.
+        </p>
+    </article>
+);
+
+export default HomeContent;
