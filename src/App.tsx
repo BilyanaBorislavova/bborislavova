ca10fa5ba79540994729d0c0ba9d9545f6b7e702
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-04-11 16:07:43 +0300

    Added router to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-
 import './styles/styles.scss';
-import Navigation from './components/navigation/navigation';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />  
-    </div>
-  );
-}
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Navigation from './components/navigation/navigation';
+import Home from './components/home/home';
+import { URLs } from './constants/constants';
+
+const { home, resume, portfolio, skills, contact } = URLs;
+
+const App = () => (
+  <main className="page-content">
+    <Navigation />
+    <BrowserRouter>
+      <Switch>
+        <Route path={home} component={Home} />
+        <Route path={resume} component={Home} />
+        <Route path={skills} component={Home} />
+        <Route path={portfolio} component={Home} />
+        <Route path={contact} component={Home} />
+      </Switch>
+    </BrowserRouter>
+  </main>
+);
 
 export default App;
