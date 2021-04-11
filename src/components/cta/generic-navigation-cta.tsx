a6e4edc0be64b0428d88188504c657301bfa1c4c
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-04-11 13:19:43 +0300

    Added custom className prop to GenericNavigatioCta.jsx

--- src/components/cta/generic-navigation-cta.tsx
+++ src/components/cta/generic-navigation-cta.tsx
@@ -2,18 +2,27 @@ import './generic-navigation-cta.scss';
 
 import * as React from 'react';
 import GenericCta from './generic-cta';
+import { getClassName } from '../../utils/string-utils';
 
 interface GenericNavigationCtaProps {
     text: string,
     href: string,
+    className?: string,
 };
 
-const GenericNavigationCta: React.FC<GenericNavigationCtaProps> = ({ text, href }) => (
-    <GenericCta
-      text={text}
-      href={href}
-      className="generic-navigation-cta"
-    />
-);
+const GenericNavigationCta: React.FC<GenericNavigationCtaProps> = ({ text, href, className = '' }) => {
+    const combinedClassName = getClassName({
+      'generic-navigation-cta': true,
+      [className]: !!className.length,
+    });
+    
+    return (
+      <GenericCta
+        text={text}
+        href={href}
+        className={combinedClassName}
+      />
+    );
+};
 
 export default GenericNavigationCta;
