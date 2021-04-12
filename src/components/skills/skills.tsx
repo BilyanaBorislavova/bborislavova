87a2ed8acae33fb02f7040416dfce976d6a06110
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-04-12 22:48:43 +0300

    Started implementing Skills component

---
+++ src/components/skills/skills.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import PageContentWrapper from '../page-content-wrapper';
+
+const Skills: React.FC<{}> = () => (
+    <PageContentWrapper title="Skills">
+
+    </PageContentWrapper>
+);
+
+export default Skills;
