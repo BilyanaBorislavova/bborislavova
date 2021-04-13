26a9c36ecb2a134882a46c161c8af59d0f6009fd
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-04-13 06:43:05 +0300

    Create coding-skills-section.tsx

---
+++ src/components/skills/coding-skills-section.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { CodingIcon } from '../icons/icons';
+import SkillWithText from './skill-with-text';
+import SkillsSection from './skills-section';
+
+const CodingSkillsSection: React.FC<{}> = () => (
+    <SkillsSection
+      title="Coding Skills"
+      icon={<CodingIcon />}
+      className="coding-skills-section"
+    >
+        <article className="coding-skills-section-content">
+            <SkillWithText
+              text="JavaScript"
+              numberToIncrementTo={70}
+            />
+        </article>
+    </SkillsSection>
+);
+
+export default CodingSkillsSection;
