9782613c3531935654ada9110e9f47c2e4125e71
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-04-13 07:05:10 +0300

    Update skills.tsx

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import PageContentWrapper from '../page-content-wrapper';
 import CodingSkillsSection from './coding-skills-section';
+import LanguagesSkillsSection from './languages-skills-section';
 
 const Skills: React.FC<{}> = () => (
     <PageContentWrapper title="Skills">
         <CodingSkillsSection />
+        <LanguagesSkillsSection />
     </PageContentWrapper>
 );
 
