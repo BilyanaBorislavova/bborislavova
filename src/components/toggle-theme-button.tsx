c8da6597976fe42d694e2926aafab154379b4bca
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-04-25 12:44:09 +0300

    Added ToggleThemeButton component

---
+++ src/components/toggle-theme-button.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import ThemeContext from '../context/theme-context';
+import { DEFAULT_THEMES } from '../providers/theme-provider';
+import { getClassName } from '../utils/string-utils';
+
+const { LIGHT, DARK } = DEFAULT_THEMES;
+
+const ToggleThemeButton: React.FC = () => {
+    const { theme, toggleTheme } = React.useContext(ThemeContext);
+
+    const isThemeLight = theme === LIGHT;
+    const isThemeDark = theme === DARK;
+
+    const customClassName = getClassName({
+        'toggle-theme-button': true,
+        'toggle-theme-button-light': isThemeLight,
+        'toggle-theme-button-dark': isThemeDark,
+    });
+
+    return (
+        <button
+          className={customClassName}
+          onClick={toggleTheme}
+        >
+            123
+        </button>
+    )
+};
+
+export default ToggleThemeButton;
