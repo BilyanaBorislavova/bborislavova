561ec23be22b44b6a8444c82874c564c8eefbfe4
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2021-04-25 12:43:54 +0300

    Used ThemeProvider in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import './styles/styles.scss';
+import "./styles/styles.scss";
 
-import * as React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import * as React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import { URLs } from './constants/constants';
-import Navigation from './components/navigation/navigation';
-import Home from './components/home/home';
-import Resume from './components/resume/resume';
-import Skills from './components/skills/skills';
-import Portfolio from './components/portfolio/portfolio';
+import { URLs } from "./constants/constants";
+import Navigation from "./components/navigation/navigation";
+import Home from "./components/home/home";
+import Resume from "./components/resume/resume";
+import Skills from "./components/skills/skills";
+import Portfolio from "./components/portfolio/portfolio";
+import ThemeProvider from "./providers/theme-provider";
+import ToggleThemeButton from "./components/toggle-theme-button";
 
-const { home, resume, portfolio, skills } = URLs;
+const { resume, portfolio, skills } = URLs;
 
 const App = () => (
-  <main className="page-content">
-    <Navigation />
-    <Home />
+  <ThemeProvider>
+    <ToggleThemeButton />
+    <main className="page-content">
+      <Navigation />
+      <Home />
       <Switch>
         <Route path={resume} component={Resume} />
         <Route path={skills} component={Skills} />
         <Route path={portfolio} component={Portfolio} />
       </Switch>
-  </main>
+    </main>
+  </ThemeProvider>
 );
 
 export default App;
